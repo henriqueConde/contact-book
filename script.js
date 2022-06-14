707367481535c259df4ce70c68c0d24c64046ed5
Henrique <ext.henrique.conde@farfetch.com>
2022-06-15 00:25:02 +0100

    chore: Add form validation

--- script.js
+++ script.js
@@ -5,6 +5,53 @@ const INITIAL_STATE = {
 let state = INITIAL_STATE;
 const MAIN = document.querySelector('#main');
 
+const FORM_FIELDS = [
+    {
+        name: 'name',
+        label: 'Name',
+        type: 'text',
+        className: 'form__name',
+        validations: {
+            isRequired: true,
+            regexVal: /^[a-z ,.'-]+$/i,
+            minLength: 2,
+            maxLength: 20,
+        }
+    },
+    {
+        name: 'surname',
+        label: 'Surname',
+        type: 'text',
+        className: 'form__surname',
+        validations: {
+            isRequired: true,
+            regexVal: /^[a-z ,.'-]+$/i,
+            minLength: 2,
+            maxLength: 20,
+        }
+    },
+    {
+        name: 'email',
+        label: 'Email',
+        type: 'email',
+        className: 'form__email',
+        validations: {
+            isRequired: true,
+            regexVal: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i,
+        }
+    },
+    {
+        name: 'phone-number',
+        label: 'Phone Number',
+        type: 'tel',
+        className: 'form__phone-number',
+        validations: {
+            isRequired: false,
+            regexVal: /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{3})(?: *x(\d+))?\s*$/i,
+        }
+    },
+];
+
 const getLocalStorageState = () => JSON.parse(localStorage.getItem('state'));
 const setLocalStorageState = newState => localStorage.setItem('state', JSON.stringify(newState));
 
@@ -21,7 +68,6 @@ const createEl = (tag, attributes = {}, events = {}) => {
 
     return el;
 }
-
 
 // Check if localStorage has state
 const checkLocalStorage = () => {
@@ -42,41 +88,65 @@ const renderAddContactModal = () => {
     })
 }
 
-const createAddContactForm = () => {
-    const formFields = [
-        {
-            name: 'name',
-            label: 'Name',
-            type: 'text',
-            className: 'form__name'
-        },
-        {
-            name: 'surname',
-            label: 'Surname',
-            type: 'text',
-            className: 'form__surname'
-        },
-        {
-            name: 'email',
-            label: 'Email',
-            type: 'email',
-            className: 'form__email'
-        },
-        {
-            name: 'phone-number',
-            label: 'Phone Number',
-            type: 'tel',
-            className: 'form__phone-number'
-        },
-    ];
+const validateForm = () => {
+    const formInputs = [...document.querySelectorAll('.form__input')];
+    const errorMessages = {};
+    formInputs.forEach(input => {
+        const { validations } = FORM_FIELDS.filter(field => field.name === input.name)[0];
 
+        if(!validations.isRequired && input.value === '') {
+            return;
+        }
+        
+        if(validations.minLength !== null && input.value.length < validations.minLength) {
+            errorMessages[input.name] = `The ${input.name} is to short`;
+            return;
+        }
+
+        if(validations.maxLength !== null && input.value.length > validations.maxLength) {
+            errorMessages[input.name] = `The ${input.name} is to long`;
+            return;
+        }
+
+        if(!validations.regexVal.test(input.value)) {
+            errorMessages[input.name] = `The ${input.name} is not valid`;
+            return;
+        }
+
+    })
+
+    return errorMessages;
+}
+
+const renderErrorMessages = errorMessages => {
+    removeErrorMessages();
+    Object.keys(errorMessages).forEach(key => {
+        const errorInput = document.querySelector(`#${key}`).parentNode;
+        const message = createEl('span', {
+            textContent: errorMessages[key],
+            className: 'form__error-message'
+        })
+        errorInput.appendChild(message);
+    })
+}
+
+const removeErrorMessages = () => {
+    const errorMessages = [...document.querySelectorAll('.form__error-message')];
+    errorMessages.forEach(message => message.remove());
+}
+
+const createAddContactForm = () => {
     const addContactForm = createEl('form', {
         className: 'form'
     });
 
-    formFields.forEach(field => {
+    FORM_FIELDS.forEach(field => {
         const formGroup = createEl('div', {
             className: 'form__group',
+        });
+
+        const divInput = createEl('div', {
+            className:'input-div',
         });
 
         const input = createEl('input', {
@@ -84,6 +154,8 @@ const createAddContactForm = () => {
             name: field.name,
             id: field.name,
             type: field.type
+        }, {
+            focus: removeErrorMessages,
         });
 
         const label = createEl('label', {
@@ -92,23 +164,45 @@ const createAddContactForm = () => {
             htmlFor: field.name
         })
 
+        divInput.appendChild(input)
         formGroup.appendChild(label);
-        formGroup.appendChild(input);
+        formGroup.appendChild(divInput);
         addContactForm.appendChild(formGroup);
     })
 
     const addContactButton = createEl('button', {
         textContent: 'Add Contact',
         className: 'form__add-button'
+    }, {
+        click: function(event) {
+            event.preventDefault();
+            const errorMessages = validateForm();
+            if (errorMessages.length === 0) {
+                // updateState();
+                // removeModal();
+            } else {
+                renderErrorMessages(errorMessages);
+            }
+        }
     })
 
     addContactForm.appendChild(addContactButton);
     return addContactForm;
 }
 
+const removeModal = () => {
+    document.querySelector('.modal-container').remove();
+}
+
 const createAddContactModal = () => {
     const modalContainer = createEl('div', {
         className: 'modal-container'
+    }, {
+        click: function ({ target }) {
+            if(target.classList.contains('modal-container')) {
+                return removeModal()
+            }
+        },
     })
 
     const modal = createEl('div', {
@@ -119,7 +213,6 @@ const createAddContactModal = () => {
     document.querySelector('#app').appendChild(modalContainer);
     modal.appendChild(createAddContactForm());
 }
-
 
 const INIT = () => {
     checkLocalStorage();
